2cc933c41f80d1b9c048322073e8d11744c57816
tacjlee <tacjlee@gmail.com>
2024-12-18 21:08:13 +0700

    Rollback fx operators

--- packages/fxoperators/fxoperators.go
+++ packages/fxoperators/fxoperators.go
@@ -1,44 +1,8 @@
 package fxoperators
 
-import "reflect"
-
-func If(condition bool, trueVal any, falseVal any) any {
-	value := falseVal
+func If[T any](condition bool, trueVal any, falseVal any) T {
 	if condition {
-		value = trueVal
+		return trueVal.(T)
 	}
-	valueType := reflect.TypeOf(value)
-	if valueType == nil {
-		return value
-	} else if valueType.Kind() == reflect.Bool {
-		return value.(bool)
-	} else if valueType.Kind() == reflect.Float64 {
-		return value.(float64)
-	} else if valueType.Kind() == reflect.Float32 {
-		return value.(float32)
-	} else if valueType.Kind() == reflect.Int {
-		return value.(int)
-	} else if valueType.Kind() == reflect.Int8 {
-		return value.(int8)
-	} else if valueType.Kind() == reflect.Int16 {
-		return value.(int16)
-	} else if valueType.Kind() == reflect.Int32 {
-		return value.(int32)
-	} else if valueType.Kind() == reflect.Int64 {
-		return value.(int64)
-	} else if valueType.Kind() == reflect.Uint {
-		return value.(uint)
-	} else if valueType.Kind() == reflect.Uint8 {
-		return value.(uint8)
-	} else if valueType.Kind() == reflect.Uint16 {
-		return value.(uint16)
-	} else if valueType.Kind() == reflect.Uint32 {
-		return value.(uint32)
-	} else if valueType.Kind() == reflect.Uint64 {
-		return value.(uint64)
-	} else if valueType.Kind() == reflect.String {
-		return value.(string)
-	} else {
-		return value
-	}
+	return falseVal.(T)
 }
