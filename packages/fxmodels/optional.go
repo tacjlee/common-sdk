d3d5af0cbbf384ddd091bbe8ea5433fe311cd3fb
tacjlee <tacjlee@gmail.com>
2025-06-26 16:30:41 +0700

    Refactor fxsql

--- packages/fxmodels/optional.go
+++ packages/fxmodels/optional.go
@@ -4,25 +4,25 @@ type Optional[T any] struct {
 	Value *T
 }
 
-func (o Optional[T]) IsPresent() bool {
+func (o *Optional[T]) IsPresent() bool {
 	return o.Value != nil
 }
 
-func (o Optional[T]) Get() T {
+func (o *Optional[T]) Get() *T {
 	// If the caller does not need to modify the returned Value
 	// If the caller needs to modify the returned Value, return a pointer.
-	return *o.Value
+	return o.Value
 }
 
-func (o Optional[T]) IsNil() bool {
+func (o *Optional[T]) IsNil() bool {
 	return o.Value == nil
 }
 
-func (o Optional[T]) GetPresentOrEmpty() T {
+func (o *Optional[T]) GetPresentOrEmpty() *T {
 	var model T
 	if o.Value != nil {
-		return *o.Value
+		return o.Value
 	} else {
-		return model
+		return &model
 	}
 }
