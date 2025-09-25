27c994d3274ca3b3f92374c5b169e09468ab6522
TacjLee <tacjlee@gmail.com>
2025-09-25 15:25:20 +0700

    Pageable supports filter

--- packages/fxmodels/pageable.go
+++ packages/fxmodels/pageable.go
@@ -1,8 +1,9 @@
 package fxmodels
 
 type Pageable struct {
-	PageNumber int    `json:"pageNumber"`
-	PageSize   int    `json:"pageSize"`
-	Offset     int64  `json:"offset"`
-	Order      string `json:"order"`
+	PageNumber int                    `json:"pageNumber"`
+	PageSize   int                    `json:"pageSize"`
+	Offset     int64                  `json:"offset"`
+	Order      string                 `json:"order"`
+	Filter     map[string]interface{} `json:"filter"`
 }
